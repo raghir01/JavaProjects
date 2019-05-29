a7c852d1a10d155d4d4362ad14bb076c2d2309db
Raghi Ramachandran <raghir01@gmail.com>
2019-05-28 23:06:06 -0400

    homework submission

---
+++ src/main/java/com/company/MarvelTable.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class MarvelTable {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------------\n" +
+                "|   Hero              |   Affiliation     |   Power         |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Captain America   |   The Avengers    |   Peak Human    |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Wolverine         |   The X-Men       |   Healing/Claws |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Cyclops           |   The X-Men       |   Eye Beam      |\n" +
+                "-------------------------------------------------------------\n" +
+                "|   Domino            |   X-Force         |   Luck          |\n" +
+                "-------------------------------------------------------------");
+    }
+}
